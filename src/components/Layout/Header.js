bee54aeaed4ca4dc09a3a592b37bb488d8b9cc87
Profana92 <stevevtek@gmail.com>
2023-02-14 22:34:52 +0100

    folder restructure

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <React.Fragment>
+      <header></header>
+      <div></div>
+    </React.Fragment>
+  );
+}
+
+export default Header;
