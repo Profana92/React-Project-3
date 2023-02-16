8b8aca30a3ca26bf9d2d65ae1da8c02d29ec7fa2
Profana92 <87131514+Profana92@users.noreply.github.com>
2023-02-16 09:55:22 +0100

    update 16,02

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Header from "./components/Layout/Header";
-import React, { Fragment } from "react";
+import React from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,14 +16,14 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
 
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
